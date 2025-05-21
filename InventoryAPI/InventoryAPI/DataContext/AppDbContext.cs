3f73b0d801d241c8afa0801649f68804e9ad3d06
Ivanncho <jovanovski.ivanco@yahoo.com>
2025-05-21 11:44:53 +0200

    Clean commit with .gitignore in place

---
+++ InventoryAPI/InventoryAPI/DataContext/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using InventoryAPI.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryAPI.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ InventoryAPI/InventoryAPI/Entity/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryAPI.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int QuantityInStock { get; set; }
+
+        public string? Category { get; set; }
+    }
+}
